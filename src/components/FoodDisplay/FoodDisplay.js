eb3f703c9c8565dd0bc985386e4145306af5097b
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-02-25 10:48:18 +0530

    Testing images link.

--- src/components/FoodDisplay/FoodDisplay.js
+++ src/components/FoodDisplay/FoodDisplay.js
@@ -3,12 +3,44 @@ import "./FoodDisplay.css";
 import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 
+const temp_list = [
+  {
+    _id: "1",
+    name: "KURKURE",
+    image:
+      "https://www.shutterstock.com/shutterstock/photos/1863139243/display_1500/stock-photo-guwahati-assam-india-november-famous-indian-snacks-brand-kurkure-masala-munch-1863139243.jpg",
+    price: 20,
+    description: "Crunchy and spicy snacks packed with bold flavors.",
+    category: "Snack Items",
+    offers: "",
+  },
+  {
+    _id: "2",
+    name: "LAYS ASCO",
+    image: "",
+    price: 20,
+    description: "Crispy and crunchy potato chips with a variety of flavors.",
+    category: "Snack Items",
+    offers: "",
+  },
+  {
+    _id: "3",
+    name: "LAYS CL",
+    image: "",
+    price: 20,
+    description: "Crispy and crunchy potato chips with a variety of flavors.",
+    category: "Snack Items",
+    offers: "",
+  },
+];
+
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
+  console.log(category);
   return (
     <div className="food-display" id="food-display">
       <h2>Top Dishes near you.</h2>
-      <div className="food-display-list">
+      {/* <div className="food-display-list">
         {food_list.map((item, index) => {
           if (category === "All" || category === item.category) {
             return (
@@ -23,6 +55,18 @@ const FoodDisplay = ({ category }) => {
             );
           }
         })}
+      </div> */}
+      <div className="food-display-list">
+        {temp_list.map((item, index) => (
+          <FoodItem
+            key={index}
+            id={item._id}
+            name={item.name}
+            description={item.description}
+            price={item.price}
+            image={item.image}
+          />
+        ))}
       </div>
     </div>
   );
