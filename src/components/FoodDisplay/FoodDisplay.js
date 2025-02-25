9f6ed5a249afdb8778040a90844c2e98bcb44665
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-02-25 12:15:55 +0530

    I dont know what the errors are.

--- src/components/FoodDisplay/FoodDisplay.js
+++ src/components/FoodDisplay/FoodDisplay.js
@@ -1,8 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./FoodDisplay.css";
 import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAbY5XvyshMyC7PZbwh-roJf1705Yjn7kI",
+  authDomain: "stanza2cart.firebaseapp.com",
+  projectId: "stanza2cart",
+  storageBucket: "stanza2cart.firebasestorage.app",
+  messagingSenderId: "234021511731",
+  appId: "1:234021511731:web:b429c948d090ff2aca7946",
+  measurementId: "G-WXEQNF0FEF",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 const temp_list = [
   {
     _id: "1",
@@ -35,6 +49,25 @@ const temp_list = [
 ];
 
 const FoodDisplay = ({ category }) => {
+  const [snackData, setSnackData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const docRef = doc(db, "food_lists", "Snack_Items");
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          setSnackData(docSnap.data());
+        } else {
+          console.log("No such document!");
+        }
+      } catch (error) {
+        console.log("Error", error);
+      }
+    };
+    fetchData();
+    console.log(snackData);
+  }, []);
   const { food_list } = useContext(StoreContext);
   console.log(category);
   return (
